3c016a363af7a44bc0a2763006ac96101af452c1
Taurhiel <thullyolima1@gmail.com>
2026-04-24 05:16:41 -0300

    Use official-style fluent fire emoji

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -82,6 +82,161 @@ const streakDays = [
 
 const masteryItems = ["Linguagens", "Matematica", "Humanas", "Natureza"];
 
+function FireEmojiIcon({ idPrefix }: { idPrefix: string }) {
+  return (
+    <svg viewBox="0 0 32 32" aria-hidden="true">
+      <path
+        fill={`url(#${idPrefix}-a)`}
+        d="M12.5554 8.93525c1.5367-2.39746 2.4462-4.61157 2.9208-6.17691.1819-.59994.9502-.84569 1.4062-.41557 6.8725 6.48124 9.1187 11.42943 9.5669 17.14803.3281 5.5313-2.3906 10.4532-9.6875 10.4532-6.85071 0-11.90633-4.7813-11.21886-12.3594.41085-4.529 2.17156-7.97922 3.57924-10.03311.4381-.6392 1.35082-.64847 1.85572-.06057l1.2591 1.46627c.0868.10103.2465.09017.3184-.02194Z"
+      />
+      <path
+        fill={`url(#${idPrefix}-b)`}
+        d="M12.5554 8.93525c1.5367-2.39746 2.4462-4.61157 2.9208-6.17691.1819-.59994.9502-.84569 1.4062-.41557 6.8725 6.48124 9.1187 11.42943 9.5669 17.14803.3281 5.5313-2.3906 10.4532-9.6875 10.4532-6.85071 0-11.90633-4.7813-11.21886-12.3594.41085-4.529 2.17156-7.97922 3.57924-10.03311.4381-.6392 1.35082-.64847 1.85572-.06057l1.2591 1.46627c.0868.10103.2465.09017.3184-.02194Z"
+      />
+      <path
+        fill={`url(#${idPrefix}-c)`}
+        d="M12.5554 8.93525c1.5367-2.39746 2.4462-4.61157 2.9208-6.17691.1819-.59994.9502-.84569 1.4062-.41557 6.8725 6.48124 9.1187 11.42943 9.5669 17.14803.3281 5.5313-2.3906 10.4532-9.6875 10.4532-6.85071 0-11.90633-4.7813-11.21886-12.3594.41085-4.529 2.17156-7.97922 3.57924-10.03311.4381-.6392 1.35082-.64847 1.85572-.06057l1.2591 1.46627c.0868.10103.2465.09017.3184-.02194Z"
+      />
+      <path
+        fill={`url(#${idPrefix}-d)`}
+        d="M12.5554 8.93525c1.5367-2.39746 2.4462-4.61157 2.9208-6.17691.1819-.59994.9502-.84569 1.4062-.41557 6.8725 6.48124 9.1187 11.42943 9.5669 17.14803.3281 5.5313-2.3906 10.4532-9.6875 10.4532-6.85071 0-11.90633-4.7813-11.21886-12.3594.41085-4.529 2.17156-7.97922 3.57924-10.03311.4381-.6392 1.35082-.64847 1.85572-.06057l1.2591 1.46627c.0868.10103.2465.09017.3184-.02194Z"
+      />
+      <path
+        fill={`url(#${idPrefix}-e)`}
+        d="M12.5554 8.93525c1.5367-2.39746 2.4462-4.61157 2.9208-6.17691.1819-.59994.9502-.84569 1.4062-.41557 6.8725 6.48124 9.1187 11.42943 9.5669 17.14803.3281 5.5313-2.3906 10.4532-9.6875 10.4532-6.85071 0-11.90633-4.7813-11.21886-12.3594.41085-4.529 2.17156-7.97922 3.57924-10.03311.4381-.6392 1.35082-.64847 1.85572-.06057l1.2591 1.46627c.0868.10103.2465.09017.3184-.02194Z"
+      />
+      <path
+        fill={`url(#${idPrefix}-f)`}
+        d="M12.5554 8.93525c1.5367-2.39746 2.4462-4.61157 2.9208-6.17691.1819-.59994.9502-.84569 1.4062-.41557 6.8725 6.48124 9.1187 11.42943 9.5669 17.14803.3281 5.5313-2.3906 10.4532-9.6875 10.4532-6.85071 0-11.90633-4.7813-11.21886-12.3594.41085-4.529 2.17156-7.97922 3.57924-10.03311.4381-.6392 1.35082-.64847 1.85572-.06057l1.2591 1.46627c.0868.10103.2465.09017.3184-.02194Z"
+      />
+      <path
+        fill={`url(#${idPrefix}-g)`}
+        d="M12.5554 8.93525c1.5367-2.39746 2.4462-4.61157 2.9208-6.17691.1819-.59994.9502-.84569 1.4062-.41557 6.8725 6.48124 9.1187 11.42943 9.5669 17.14803.3281 5.5313-2.3906 10.4532-9.6875 10.4532-6.85071 0-11.90633-4.7813-11.21886-12.3594.41085-4.529 2.17156-7.97922 3.57924-10.03311.4381-.6392 1.35082-.64847 1.85572-.06057l1.2591 1.46627c.0868.10103.2465.09017.3184-.02194Z"
+      />
+      <path
+        fill={`url(#${idPrefix}-h)`}
+        d="M12.5554 8.93525c1.5367-2.39746 2.4462-4.61157 2.9208-6.17691.1819-.59994.9502-.84569 1.4062-.41557 6.8725 6.48124 9.1187 11.42943 9.5669 17.14803.3281 5.5313-2.3906 10.4532-9.6875 10.4532-6.85071 0-11.90633-4.7813-11.21886-12.3594.41085-4.529 2.17156-7.97922 3.57924-10.03311.4381-.6392 1.35082-.64847 1.85572-.06057l1.2591 1.46627c.0868.10103.2465.09017.3184-.02194Z"
+      />
+      <g filter={`url(#${idPrefix}-i)`}>
+        <path
+          fill={`url(#${idPrefix}-j)`}
+          d="M10.1782 19.8769c1.3023-3.0993 3.9746-6.5289 5.7792-8.3894.5099-.5257 1.3341-.5351 1.8727-.0388 3.4612 3.189 4.5031 6.072 5.3873 9.4282.9198 3.4917 0 9.1177-6.9216 9.1177-5.9607 0-8.02561-5.5772-6.1176-10.1177Z"
+        />
+        <path
+          fill={`url(#${idPrefix}-k)`}
+          d="M10.1782 19.8769c1.3023-3.0993 3.9746-6.5289 5.7792-8.3894.5099-.5257 1.3341-.5351 1.8727-.0388 3.4612 3.189 4.5031 6.072 5.3873 9.4282.9198 3.4917 0 9.1177-6.9216 9.1177-5.9607 0-8.02561-5.5772-6.1176-10.1177Z"
+        />
+        <path
+          fill={`url(#${idPrefix}-l)`}
+          d="M10.1782 19.8769c1.3023-3.0993 3.9746-6.5289 5.7792-8.3894.5099-.5257 1.3341-.5351 1.8727-.0388 3.4612 3.189 4.5031 6.072 5.3873 9.4282.9198 3.4917 0 9.1177-6.9216 9.1177-5.9607 0-8.02561-5.5772-6.1176-10.1177Z"
+        />
+      </g>
+      <path
+        fill={`url(#${idPrefix}-m)`}
+        d="M10.1782 19.8769c1.3023-3.0993 3.9746-6.5289 5.7792-8.3894.5099-.5257 1.3341-.5351 1.8727-.0388 3.4612 3.189 4.5031 6.072 5.3873 9.4282.9198 3.4917 0 9.1177-6.9216 9.1177-5.9607 0-8.02561-5.5772-6.1176-10.1177Z"
+      />
+      <path
+        fill={`url(#${idPrefix}-n)`}
+        d="M10.1782 19.8769c1.3023-3.0993 3.9746-6.5289 5.7792-8.3894.5099-.5257 1.3341-.5351 1.8727-.0388 3.4612 3.189 4.5031 6.072 5.3873 9.4282.9198 3.4917 0 9.1177-6.9216 9.1177-5.9607 0-8.02561-5.5772-6.1176-10.1177Z"
+      />
+      <g filter={`url(#${idPrefix}-o)`}>
+        <path
+          fill={`url(#${idPrefix}-p)`}
+          d="M11.8137 11.5385c2.3154-3.38999 3.6694-7.05706 4.0829-7.96197-.603 4.6348-2.3602 8.02817-5.6679 12.39807-2.64616 3.496-3.12394 6.4888-3.10096 7.2171-.82693-5.5617 1.79167-7.4157 4.68596-11.6532Z"
+        />
+      </g>
+      <g filter={`url(#${idPrefix}-q)`}>
+        <path
+          fill={`url(#${idPrefix}-r)`}
+          d="M9.81366 7.87422C8.5136 9.4879 5.9295 13.9638 5.99348 18.9582c1.28144-5.1224 4.27572-6.7367 3.82018-11.08398Z"
+        />
+      </g>
+      <defs>
+        <radialGradient id={`${idPrefix}-a`} cx="0" cy="0" r="1" gradientTransform="matrix(-17.09808 -.15697 .23672 -25.78501 24.0023 19.72)" gradientUnits="userSpaceOnUse">
+          <stop stopColor="#FF953D" />
+          <stop offset="1" stopColor="#FF5141" />
+        </radialGradient>
+        <radialGradient id={`${idPrefix}-b`} cx="0" cy="0" r="1" gradientTransform="matrix(-9.58662 -3.88551 6.70473 -16.5424 10.3724 15.6549)" gradientUnits="userSpaceOnUse">
+          <stop stopColor="#CE5327" />
+          <stop offset="1" stopColor="#CE5327" stopOpacity="0" />
+        </radialGradient>
+        <radialGradient id={`${idPrefix}-d`} cx="0" cy="0" r="1" gradientTransform="matrix(2.83591 1.26351 -10.45887 23.47458 4.95718 14.3914)" gradientUnits="userSpaceOnUse">
+          <stop stopColor="#FFAA7B" />
+          <stop offset="1" stopColor="#FFAA7B" stopOpacity="0" />
+        </radialGradient>
+        <radialGradient id={`${idPrefix}-e`} cx="0" cy="0" r="1" gradientTransform="matrix(.843 3.74668 -4.675 1.05188 9.31032 6.25095)" gradientUnits="userSpaceOnUse">
+          <stop stopColor="#FF5E47" />
+          <stop offset="1" stopColor="#FF5E47" stopOpacity="0" />
+        </radialGradient>
+        <radialGradient id={`${idPrefix}-f`} cx="0" cy="0" r="1" gradientTransform="matrix(.37467 10.13047 -9.3768 .3468 16.429 1.36584)" gradientUnits="userSpaceOnUse">
+          <stop stopColor="#FF2F3C" />
+          <stop offset="1" stopColor="#FF2F3C" stopOpacity="0" />
+        </radialGradient>
+        <radialGradient id={`${idPrefix}-g`} cx="0" cy="0" r="1" gradientTransform="matrix(2.07795 .9835 -1.9737 4.17002 13.9 4.79911)" gradientUnits="userSpaceOnUse">
+          <stop stopColor="#FF846C" />
+          <stop offset="1" stopColor="#FF846C" stopOpacity="0" />
+        </radialGradient>
+        <radialGradient id={`${idPrefix}-h`} cx="0" cy="0" r="1" gradientTransform="matrix(-.89842 2.09375 -.4798 -.20588 12.4577 8.20959)" gradientUnits="userSpaceOnUse">
+          <stop stopColor="#FFA682" />
+          <stop offset="1" stopColor="#FFA682" stopOpacity="0" />
+        </radialGradient>
+        <radialGradient id={`${idPrefix}-j`} cx="0" cy="0" r="1" gradientTransform="matrix(-9.82978 -1.98953 2.47754 -12.2409 21.2046 24.3762)" gradientUnits="userSpaceOnUse">
+          <stop stopColor="#FFDA2F" />
+          <stop offset="1" stopColor="#FF8E41" />
+        </radialGradient>
+        <radialGradient id={`${idPrefix}-k`} cx="0" cy="0" r="1" gradientTransform="matrix(5.05803 13.20707 -11.47514 4.39474 12.4013 8.59263)" gradientUnits="userSpaceOnUse">
+          <stop stopColor="#FD5639" />
+          <stop offset="1" stopColor="#FE5533" stopOpacity="0" />
+        </radialGradient>
+        <radialGradient id={`${idPrefix}-l`} cx="0" cy="0" r="1" gradientTransform="matrix(-9.74917 .98358 -2.40823 -23.87023 19.239 20.6289)" gradientUnits="userSpaceOnUse">
+          <stop offset=".627719" stopColor="#D7812D" stopOpacity="0" />
+          <stop offset="1" stopColor="#D7812D" />
+        </radialGradient>
+        <radialGradient id={`${idPrefix}-n`} cx="0" cy="0" r="1" gradientTransform="matrix(-12.83239 9.6478 -6.98132 -9.28575 22.9857 18.8023)" gradientUnits="userSpaceOnUse">
+          <stop offset=".772305" stopColor="#F18A52" stopOpacity="0" />
+          <stop offset="1" stopColor="#F18A52" />
+        </radialGradient>
+        <linearGradient id={`${idPrefix}-c`} x1="18.3364" x2="18.3364" y1="29.944" y2="24.8455" gradientUnits="userSpaceOnUse">
+          <stop stopColor="#FF7583" />
+          <stop offset="1" stopColor="#FF7583" stopOpacity="0" />
+        </linearGradient>
+        <linearGradient id={`${idPrefix}-m`} x1="16.5026" x2="16.5026" y1="10.6122" y2="14.2595" gradientUnits="userSpaceOnUse">
+          <stop stopColor="#F95131" />
+          <stop offset="1" stopColor="#F95131" stopOpacity="0" />
+        </linearGradient>
+        <linearGradient id={`${idPrefix}-p`} x1="14.9957" x2="7.65549" y1="4.2552" y2="22.7319" gradientUnits="userSpaceOnUse">
+          <stop stopColor="#FF7558" />
+          <stop offset="1" stopColor="#F38758" />
+        </linearGradient>
+        <linearGradient id={`${idPrefix}-r`} x1="9.54097" x2="5.58208" y1="8.14373" y2="19.4793" gradientUnits="userSpaceOnUse">
+          <stop stopColor="#FF815B" />
+          <stop offset="1" stopColor="#FF9C6D" />
+        </linearGradient>
+        <filter id={`${idPrefix}-i`} width="14.5255" height="18.9099" x="9.48987" y="11.0846" colorInterpolationFilters="sRGB" filterUnits="userSpaceOnUse">
+          <feFlood floodOpacity="0" result="BackgroundImageFix" />
+          <feBlend in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
+          <feColorMatrix in="SourceAlpha" result="hardAlpha" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" />
+          <feOffset dx=".5" />
+          <feGaussianBlur stdDeviation=".25" />
+          <feComposite in2="hardAlpha" k2="-1" k3="1" operator="arithmetic" />
+          <feColorMatrix values="0 0 0 0 0.952941 0 0 0 0 0.615686 0 0 0 0 0.364706 0 0 0 1 0" />
+          <feBlend in2="shape" result="effect1_innerShadow_18_15821" />
+        </filter>
+        <filter id={`${idPrefix}-o`} width="11.4237" height="22.1152" x="5.7229" y="2.32654" colorInterpolationFilters="sRGB" filterUnits="userSpaceOnUse">
+          <feFlood floodOpacity="0" result="BackgroundImageFix" />
+          <feBlend in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
+          <feGaussianBlur result="effect1_foregroundBlur_18_15821" stdDeviation=".625" />
+        </filter>
+        <filter id={`${idPrefix}-q`} width="6.86804" height="14.0839" x="4.49231" y="6.37427" colorInterpolationFilters="sRGB" filterUnits="userSpaceOnUse">
+          <feFlood floodOpacity="0" result="BackgroundImageFix" />
+          <feBlend in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
+          <feGaussianBlur result="effect1_foregroundBlur_18_15821" stdDeviation=".75" />
+        </filter>
+      </defs>
+    </svg>
+  );
+}
+
 function HoverPanelButton({
   icon,
   panelClassName,
@@ -129,10 +284,7 @@ export default function App() {
           <div className="app-topbar-actions">
             <HoverPanelButton
               icon={
-                <svg viewBox="0 0 24 24" aria-hidden="true">
-                  <path d="M13.2 2.2s1.3 2.43.2 4.4c-.5.9-.48 1.88.04 2.74 1.58-.53 2.78-1.98 2.92-3.79 2.49 1.83 4.14 4.5 4.14 7.53 0 4.82-3.47 8.22-8.38 8.22C7.31 21.3 4 17.95 4 13.57c0-3.02 1.61-5.75 4.15-7.78.08 1.5.82 2.93 2 3.7.2-1.7 1.06-3.35 2.68-5.13.9-.99.37-2.16.37-2.16z" fill="#FF8A34" />
-                  <path d="M12.3 11.2c1.85 0 3.2 1.44 3.2 3.38 0 1.99-1.43 3.4-3.42 3.4-2.08 0-3.58-1.5-3.58-3.54 0-1.62 1-2.98 2.45-3.95.04.74.45 1.42 1.08 1.87.09-.42.24-.81.52-1.16.42-.53.58-1 .64-1.42.41.39.75.86 1.02 1.42l-.9.62a2.8 2.8 0 00-1.01-1.38z" fill="#FFCF59" />
-                </svg>
+                <FireEmojiIcon idPrefix="fire-trigger" />
               }
               panelClassName="topbar-hover-panel-compact"
             >
@@ -143,9 +295,7 @@ export default function App() {
                     <p>Responda um bloco de questoes por dia para comecar sua sequencia.</p>
                   </div>
                   <div className="topbar-stat-icon topbar-stat-icon-fire" aria-hidden="true">
-                    <svg viewBox="0 0 24 24">
-                      <path d="M13.5 2s1.2 3-.4 5.3c-1.1 1.6-1.4 2.8-.8 4 .7-.4 1.2-1 1.5-1.9 2.7 1.5 5.2 4.4 5.2 8 0 4-2.9 6.6-6.8 6.6-4.2 0-7.2-2.8-7.2-6.8 0-3.7 2.2-6.6 4.9-8.7.1 1 .5 1.8 1.1 2.4.2-1.8 1-3.4 2.5-5.2C14.8 4.5 13.5 2 13.5 2z" />
-                    </svg>
+                    <FireEmojiIcon idPrefix="fire-panel" />
                   </div>
                 </div>
 
